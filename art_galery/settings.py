38ef18b5f3d653f9ba4cd76748120195213ae868
dkoldyaev <dkoldyaev@gmail.com>
2015-04-04 23:08:51 +0500

    подключение mysql

--- art_galery/settings.py
+++ art_galery/settings.py
@@ -66,9 +66,13 @@ WSGI_APPLICATION = 'art_galery.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   'default': {
+       'ENGINE':    'django.db.backends.mysql',
+       'NAME':      'art_galery',
+       'USER':      'root',
+       'PASSWORD':  'sukasuka',
+       'HOST':      '127.0.0.1',
+       'PORT':      '3306'
     }
 }
 
